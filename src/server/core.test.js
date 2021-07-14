8e72271c378e6dc656970265ffa5dd7b7188abf5
NisarCoderHive <nisarapt@gmail.com>
2021-07-14 10:47:14 +0530

    Refactored Changes

--- src/server/core.test.js
+++ src/server/core.test.js
@@ -2,35 +2,22 @@ const core = require('./core');
 
 let matches = [{id : 1 , season : 2015, winner : 'Hyderabad'},
                {id : 2 , season : 2015, winner : 'Pune'},
-               {id : 3 , season : 2015, winner : 'Hyderabad'}]
+               {id : 3 , season : 2016, winner : 'Hyderabad'}]
 let deliveries = [
     {match_id : 1 ,bowler : 'Jadeja',extra_runs :4, bowling_team : 'Hyderabad'},
     {match_id : 1 ,bowler : 'Jadeja',extra_runs :4, bowling_team : 'Hyderabad'},
     {match_id : 2 ,bowler : 'Polard',extra_runs :4, bowling_team : 'Pune'},
 ];
-
-
 test("should return {2015:3} when supplied with 3 matches data for season as 2015",()=>{
-    expect(core.matchesPlayedPerYear(matches)).toStrictEqual({"2015":3})
+    expect(core.matchesPlayedPerYear(matches)).toStrictEqual({"2015":2,"2016":1})
 });
-
-test('Matches won per Team testing',()=>{
-    expect(core.matchesWonPerTeam(matches)).toEqual({
-        "2015":{"Hyderabad":2,"Pune":1}
-    })
-})
 
 test('should throw an error if the matches object doesnot contain season property',()=>{
-     expect(()=>{
-        const matches = [{id : 1 , winner : 'Hyderabad'}]
-        core.matchesPlayedPerYear(matches)}).toThrow("No Season Property")
+    expect(()=>{
+       const matches = [{id : 1 , winner : 'Hyderabad'}]
+       core.matchesPlayedPerYear(matches)}).toThrow("No Season Property")
 });
 
-test('Testing Extra runs conceded output',()=>{
-    expect(core.runsConcededPerTeam(matches,deliveries,2015)).toEqual({
-     "Hyderabad":8,"Pune":4    
-    })
-})
 
 test("should throw an error if called without an arg", () => {
     expect( ()=> {core.matchesPlayedPerYear()}).toThrow("No Matches Data Passed")
@@ -40,44 +27,33 @@ test("should throw an error if called without Matches data", () => {
     expect( ()=>{core.matchesPlayedPerYear([])}).toThrow("No Matches Data Passed")
 }); 
 
+test('Matches won per Team testing',()=>{
+    expect(core.matchesWonPerTeam(matches)).toEqual({
+        "2015":{"Hyderabad":2,"Pune":1}
+    })
+})
 
 test("should throw an eroor if called without an argument",()=>{
     expect(()=>{core.matchesWonPerTeam()}).toThrow("No Matches Data Passed")
 }) 
 
 
-test("Matcheswonperteam - should throw an error if called with Empty matches data",()=>{
+test("Matches won per team - should throw an error if called with Empty matches data",()=>{
     expect(()=>{ core.matchesWonPerTeam([]) } ).toThrow("No Matches Data Passed");
-}
-)
-/*
+})
+
+test('Testing Extra runs conceded output',()=>{
+    expect(core.runsConcededPerTeam(matches,deliveries,2015)).toEqual({
+     "Hyderabad":8,"Pune":4    
+    })
+})
+
+test('Testing Extra runs conceded output',()=>{
+    expect(()=>{core.runsConcededPerTeam()}).toThrow("No Matches , No deliveries Data ")
+    })
+
+
 test("should throw an error if called without an arg", () => {
-    expect( ()=> {core.matchesPlayedPerYear()}).toThrow("No Matches Data Passed")
+    expect( ()=> {core.topTenBowlers()}).toThrow("No Matches , No Deliveries Data Passed")
   })
 
-test("should throw an error if called without Matches data", () => {
-    expect( ()=>{core.matchesPlayedPerYear([])}).toThrow("No Matches Data Passed")
-});
-test("should throw an error if called without an arg", () => {
-    expect( ()=> {core.matchesPlayedPerYear()}).toThrow("No Matches Data Passed")
-  })
-
-test("should throw an error if called without Matches data", () => {
-    expect( ()=>{core.matchesPlayedPerYear([])}).toThrow("No Matches Data Passed")
-});
-test("should throw an error if called without an arg", () => {
-    expect( ()=> {core.matchesPlayedPerYear()}).toThrow("No Matches Data Passed")
-  })
-
-test("should throw an error if called without Matches data", () => {
-    expect( ()=>{core.matchesPlayedPerYear([])}).toThrow("No Matches Data Passed")
-});
-test("should throw an error if called without an arg", () => {
-    expect( ()=> {core.matchesPlayedPerYear()}).toThrow("No Matches Data Passed")
-  })
-
-test("should throw an error if called without Matches data", () => {
-    expect( ()=>{core.matchesPlayedPerYear([])}).toThrow("No Matches Data Passed")
-});
-
-*/
