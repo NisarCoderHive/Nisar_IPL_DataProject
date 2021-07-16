1ca33636a58362eecb341d03a101423d0a1e31f2
NisarCoderHive <nisarapt@gmail.com>
2021-07-16 10:41:13 +0530

    After adding pathmodules" ; /

--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,47 @@
 const fs = require('fs');
 
+const path = require('path');
+
 const csv = require('csvtojson');
 
 const getdata = require('./core');
 
 
+
 csv()
-.fromFile('../data/matches.csv') // Promise To Retrieve the matches data 
+.fromFile(path.resolve(__dirname,'../data/matches.csv')) // Promise To Retrieve the matches data 
 .then((matches)=>{
   //write to matchesplayedperyear.json
    try{
         let output = getdata.matchesPlayedPerYear(matches);
-        storeToJson(output,'../output/matchesPlayedPerYear.json')
+        storeToJson(output, path.resolve(__dirname,'../output/matchesPlayedPerYear.json'))
         output = getdata.matchesWonPerTeam(matches)
-        storeToJson(output,'../output/matchesWonPerTeamPerYear.json')
+        storeToJson(output, path.resolve(__dirname,'../output/matchesWonPerTeamPerYear.json'))
         }catch (err) {
         console.error(err)
       }
       csv()
-      .fromFile('../data/deliveries.csv') // Promise To Retrieve the deliveries data
+      .fromFile(path.resolve(__dirname,'../data/deliveries.csv')) // Promise To Retrieve the deliveries data
       .then((deliveries)=>{
         try{
            let output = getdata.runsConcededPerTeam(matches,deliveries,2016);
-           storeToJson(output, '../output/extraRunsConcededPerTeam.json')
+           storeToJson(output, path.resolve(__dirname,'../output/extraRunsConcededPerTeam.json'))
            output = getdata.topTenBowlers(matches,deliveries,2016);
-           storeToJson(output, '../output/topTenBowlers.json')
+           storeToJson(output, path.resolve(__dirname,'../output/topTenBowlers.json'))
         }catch(err){
            console.log(err)
         }
    });
 });
 // Function to store json data
+
+const  resolvepath = (dirname,location)=>
+{
+
+}
 const  storeToJson = (data,location)=>{
     try {
-         fs.writeFile(location, JSON.stringify(data),()=>{console.log(location +"fIleCreated")})
+         fs.writeFile(location, JSON.stringify(data),(err)=>{console.log(err)})
       } catch (err) {
         console.error(err);
       }
