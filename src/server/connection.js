6962236a6b980c3fd8bdce2d9e6b2f4b85f21004
NisarCoderHive <nisarapt@gmail.com>
2021-07-17 13:11:37 +0530

    commiting sql

---
+++ src/server/connection.js
@@ -0,0 +1,11 @@
+const connection ={
+    host: "localhost",
+    user: "postgres",
+    database: "exercises",
+    password: "postgres",
+    port: 5432,
+    idleTimeoutMillis: 0,
+    connectionTimeoutMillis: 50000
+  }
+
+  module.exports = connection;
